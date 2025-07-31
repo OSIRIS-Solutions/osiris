1219b691d6fe098e7f82d8eebb0cea469b7633a0
JKoblitz <julia.koblitz@osiris-solutions.de>
2025-07-31 11:58:45 +0200

    refactor: add note to events to highlight that they are not an activity. improve quote styling

--- pages/events/edit.php
+++ pages/events/edit.php
@@ -19,6 +19,13 @@ if (!empty($form ?? []) && isset($form['_id'])) {
         <?= lang('Add event', 'Event hinzufügen') ?>
     </h4>
 
+    <blockquote>
+        <i class="ph ph-info text-primary"></i>
+        <b><?=lang('Note:', 'Anmerkung:')?></b>
+        <?=lang('Here you can create events such as conferences, workshops or other events. These are created centrally and can be viewed by other users. It is then easy to add contributions such as presentations or posters to these events. <b>An event is not an activity!</b> It is not assigned to a person and is not a service in itself. The event is only used for the central administration of events and for linking to contributions.', 
+        'Hier kannst du Veranstaltungen, wie Konferenzen, Workshops oder andere Events anlegen. Diese werden zentral angelegt und können von anderen Nutzenden gesehen werden. Es ist danach einfach möglich, Beiträge wie zum Beispiel Vorträge oder Poster zu diesen Veranstaltungen hinzuzufügen. <b>Ein Event ist keine Aktivität!</b> Es wird keiner Person zugeordnet und ist auch an sich keine Leistung. Das Event dient lediglich der zentralen Verwaltung von Veranstaltungen und der Verknüpfung mit Beiträgen.')?>
+    </blockquote>
+
     <form action="<?= $action ?>" method="post" id="conference-form">
 
         <div class="form-group floating-form">
