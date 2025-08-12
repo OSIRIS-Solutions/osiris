3fa96cfdbd028483066a3df62258b2ac53aae875
JKoblitz <julia.koblitz@osiris-solutions.de>
2025-08-12 13:45:32 +0200

    fix: #171 error message if someone tries to add the same module again

--- js/admin-categories.js
+++ js/admin-categories.js
@@ -37,15 +37,30 @@ function sanitizeID(element, idlist = '#IDLIST li') {
 
 function addModule() {
 
-    var el = $('.author-widget')
-    var val = el.find('.module-input').val()
-    if (val === undefined || val === null) return;
+    var el = $('.author-list')
+    var val = $('.module-input').val()
     console.log(val);
+    if (val === undefined || val === null || val == '') return;
+
+    // check if value already exists
+    let values = el.find('input[type=hidden]')
+    let exists = false;
+    values.each(function () {
+        if ($(this).val() === val) {
+            exists = true;
+            return false; // break the loop
+        }
+    });
+    if (exists) {
+        toastError(lang('Module already exists.', 'Modul existiert bereits.'));
+        return;
+    }
+    
     var author = $('<div class="author" ondblclick="toggleRequired(this)">')
         .html(val);
     author.append('<input type="hidden" name="values[modules][]" value="' + val + '">')
     author.append('<a onclick="$(this).parent().remove()">&times;</a>')
-    author.appendTo(el.find('.author-list'))
+    author.appendTo(el)
 }
 
 // function addModule(type, subtype) {
