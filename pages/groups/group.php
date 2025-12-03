f7bec4960fd77a3739fe1c5a5e714baafc3fe091
JKoblitz <julia.koblitz@osiris-solutions.de>
2025-12-03 06:23:55 +0100

    improve layout

--- pages/groups/group.php
+++ pages/groups/group.php
@@ -253,11 +253,12 @@ $count_wordcloud = 0;
                 <?php } ?>
             <?php } ?>
 
+            <?php if ($level != 0) { ?>
             <a onclick="navigate('collab')" id="btn-collab" class="btn">
                 <i class="ph ph-users-three" aria-hidden="true"></i>
                 <?= lang('Other units', 'Andere Einheiten')  ?>
             </a>
-
+            <?php } ?>
 
     </nav>
 
@@ -302,7 +303,7 @@ $count_wordcloud = 0;
         <?php } ?>
 
         <div class="row row-eq-spacing">
-            <div class="col-md-3">
+            <div class="col-lg-3 order-last order-lg-first">
                 <table class="table">
                     <tbody>
                         <tr>
@@ -356,7 +357,7 @@ $count_wordcloud = 0;
                     </tbody>
                 </table>
             </div>
-            <div class="col">
+            <div class="col-lg-9">
                 <?php
                 $head = $group['head'] ?? [];
                 if (is_string($head)) $head = [$head];
