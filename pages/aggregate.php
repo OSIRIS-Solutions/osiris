d2bcf036a1c925eb47d54ec511b3424b1d230441
juwitte <juwi93@gmx.de>
2026-08-04 11:34:04 +0200

    basic functinality working

--- pages/aggregate.php
+++ pages/aggregate.php
@@ -29,7 +29,7 @@ $Format = new Document(true);
             </select>
             <br>
             <h3 class="title"><?= lang('Pipeline', 'Pipeline') ?></h3>
-            <textarea name="pipeline" id="pipeline" cols="30" rows="5" class="form-control"></textarea>
+            <textarea name="pipeline" id="pipeline" cols="30" rows="10" class="form-control"></textarea>
             <br>
         </div>
 
@@ -38,114 +38,49 @@ $Format = new Document(true);
             <div class="btn-toolbar">
                 <button class="btn secondary" onclick="getResult()"><i class="ph ph-magnifying-glass"></i> <?= lang('Apply', 'Anwenden') ?></button>
             </div>
+            
         </div>
     </div>
 
 
-    <table class="table" id="data-table">
-        <thead></thead>
-        <tbody></tbody>
-    </table>
+    <div class="box">
+        <div class="content" id="data-statistics">
+            -
+        </div>
+        <div class="footer">
+            <div class="btn-toolbar">
+                <button class="btn secondary" onclick="$('#data-raw').toggle()"><i class="ph ph-eye"></i> <?= lang('Show raw data', 'Rohdaten anzeigen') ?></button>
+                <button class="btn secondary" onclick="downloadData()"><i class="ph ph-download"></i> <?= lang('Download data', 'Herunterladen') ?></button>
+            </div>
+        </div>
+        <div class="content" id="data-raw" style="display: none;">
+            -
+        </div>
+    </div>
 
     <script>
-        var dataTable;
+        // Funktion zum Darstellen der Ergebnisse in der data-box
+        function initializeData(data) {
+            const dataStatistics = document.getElementById('data-statistics');
+            dataStatistics.innerHTML = '<p><?= lang('Results', 'Ergebnisse') ?>: ' + data.length + '</p>';
 
-        function initializeTable(data) {
-            // destroy existing table
-            if ($.fn.DataTable.isDataTable('#data-table')) {
-                $('#data-table').DataTable().clear().destroy();
-                $('#data-table thead').empty(); // Header leeren
-                $('#data-table tbody').empty(); // Daten leeren
+            const dataRaw = document.getElementById('data-raw');
+            dataRaw.innerHTML = '';
+            if (data.length === 0) {
+                dataRaw.innerHTML = '<p><?= lang('No results found', 'Keine Ergebnisse gefunden') ?></p>';
+                return;
             }
-
-            // Extrahiere die Spaltennamen aus der API-Antwort
-            const first_row = Object.keys(data[0]);
-
-
-            // Generiere dynamisch das thead
-            const thead = document.querySelector('#data-table thead');
-            const headerRow = document.createElement('tr');
-
-            var columns = [];
-        
-            var selected_columns = []
-            var array_columns = {}
-            $('#column-select input:checked').each(function() {
-                var id = $(this).data('column')
-                selected_columns.push(id)
-                var name = $(this).next('label').text()
-                const th = document.createElement('th');
-                th.textContent = name; // Optional: Titel formatieren
-                headerRow.appendChild(th);
-            })
-            thead.appendChild(headerRow);
-            // Konfiguriere die Spalten für Datatables
-            columns = selected_columns.map(function(field) {
-                // remove from selected columns
-                selected_columns = selected_columns.filter(column => column !== field);
-                // get name from `fields`
-                const filter = fields.find(f => f.id == field);
-                var r = {
-                    data: field,
-                    title: filter ? filter.label : field,
-                    defaultContent: '-'
-                }
-                if (field == 'id') {
-                    r.render = function(data, type, row, meta) {
-                        return `<a href="<?= ROOTPATH ?>/<?= $collection ?>/view/${data}"><i class="ph ph-arrow-fat-line-right"></i></a>`
-                    }
-                } else if (field == 'username') {
-                    r.render = function(data, type, row, meta) {
-                        return data ? `<a href="<?= ROOTPATH ?>/profile/${data}">${data}</a>` : '-';
-                    }
-                } else if (array_columns[field]) {
-                    var array_column = array_columns[field]
-                    r.render = function(data, type, row, meta) {
-                        if (Array.isArray(data)) {
-                            data = data.map(item => item[array_column]).join(', ');
-                        }
-                        if (data === undefined || data === null) {
-                            return '-'
-                        }
-                        if (Array.isArray(data[array_column])) {
-                            return data[array_column].join(', ') ?? data;
-                        }
-                        return data[array_column] ?? data;
+            cleanData = data.map(item => {
+                const cleanedItem = {};
+                for (const key in item) {
+                    if (key !== 'rendered' && key !== 'history') { // Exclude the _id, rendered, and history fields
+                        cleanedItem[key] = item[key];
                     }
                 }
-                return r
+                return cleanedItem;
             });
-            if (selected_columns.length > 0) {
-                toastWarning(lang('The following columns are not found in the result and are not shown:', 'Die folgenden Spalten waren im Ergebnis komplett leer und werden nicht gezeigt:') + ' <strong>' + selected_columns.join(', ') + '</strong>');
-            }
+            dataRaw.innerHTML = '<pre>' + JSON.stringify(cleanData, null, 2) + '</pre>';
 
-        
-
-            // Initialisiere Datatables
-            $('#data-table').DataTable({
-                destroy: true, // Alte Tabelle entfernen, falls sie existiert
-                data: data, // Daten direkt übergeben
-                columns: columns, // Dynamisch generierte Spalten
-                dom: 'fBrtip',
-                buttons: [{
-                    extend: 'excelHtml5',
-                    exportOptions: {
-                        columns: ':visible,:hidden' // Include hidden columns
-                    },
-                    className: 'btn small',
-                    title: "OSIRIS Search",
-                    text: '<i class="ph ph-file-xls"></i> Excel'
-                }],
-                initComplete: function() {
-                    var tableWidth = $('#data-table').width();
-                    var containerWidth = $('#data-table').parent().width();
-                    if (tableWidth > containerWidth && !$('#data-table').parent().hasClass('table-responsive')) {
-                        $('#data-table').wrap('<div class="table-responsive"></div>');
-                    }
-                }
-            });
-
-            // check if table exceeds the width of the container
         }
 
         // AJAX-Call zum Abrufen der Daten
@@ -174,19 +109,37 @@ $Format = new Document(true);
             })
             .then(response => {
                 if (!response.ok) {
-                    throw new Error(`HTTP ${response.status}`);
+                    return response.json().then(errData => {
+                        throw new Error(errData.error || 'Error');
+                    });
                 }
                 return response.json();
             })
             .then(data => {
                 console.log(data);
-                initializeTable(data.results);
+                initializeData(data.results);
             })
             .catch(err => {
-                console.error(err);
+                console.log(err);
             });
         }
 
+        function downloadData() {
+            var dataRaw = document.getElementById('data-raw');
+            if (dataRaw.innerHTML.trim() === '') {
+                toastError(lang('No data to download', 'Keine Daten zum Herunterladen'));
+                return;
+            }
+            var blob = new Blob([dataRaw.innerText], { type: 'application/json' });
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = 'data.json';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        }
     </script>
 
 </div>
